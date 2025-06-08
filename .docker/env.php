620ea2aee0308487ffc0e922568f0c127fae3ffe
jamacio <jamacio@hotmail.com>
2025-06-08 11:03:19 -0300

    update code

--- .docker/env.php
+++ .docker/env.php
@@ -97,7 +97,7 @@ return [
         'vertex' => 1
     ],
     'downloadable_domains' => [
-        'localhost',
+        'ldvdigital.com.br',
     ],
     'install' => [
         'date' => 'Wed, 03 Feb 2021 14:37:01 +0000'
@@ -123,18 +123,17 @@ return [
                     'cors_allow_credentials' => 1
                 ],
                 'unsecure' => [
-                    'base_url' => 'http://localhost/',
-                    'base_media_url' => 'http://localhost/media/'
+                    'base_url' => 'http://ldvdigital.com.br/',
+                    'base_media_url' => 'http://ldvdigital.com.br/media/'
                 ],
                 'secure' => [
-                    'base_url' => 'https://localhost/',
-                    'base_media_url' => 'https://localhost/media/',
+                    'base_url' => 'http://ldvdigital.com.br/',
+                    'base_media_url' => 'http://ldvdigital.com.br/media/',
                     'use_in_frontend' => '1',
                     'use_in_adminhtml' => '1',
-                    'base_url_frontend' => 'http://localhost:3000'
                 ],
                 'cookie' => [
-                    'cookie_domain' => 'localhost'
+                    'cookie_domain' => 'ldvdigital.com.br'
                 ]
             ],
             'catalog' => [
@@ -150,7 +149,7 @@ return [
         ]
     ],
     'http_cache_hosts' => [
-        'host' => 'localhost',
+        'host' => 'ldvdigital.com.br',
         'port' => '80'
     ],
     'cron_consumers_runner' => [
